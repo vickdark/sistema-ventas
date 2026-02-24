b9f6d76d5a2594d428625a9e527a7b7127cf5cdb
vickdark <victormanjarres3mayo@gmail.com>
2026-02-24 10:25:12 -0500

    Hazaña (arrendamiento): agregue el programa previo de arrendamiento de sesión a la configuración
    Habilite el alcance de la sesión para aplicaciones multiinquilino incluyendo SessionTenancyBootstrapper. Esto garantiza el aislamiento de los inquilinos para los datos de la sesión.

--- config/tenancy.php
+++ config/tenancy.php
@@ -35,6 +35,7 @@ return [
         Stancl\Tenancy\Bootstrappers\CacheTenancyBootstrapper::class,
         Stancl\Tenancy\Bootstrappers\FilesystemTenancyBootstrapper::class,
         Stancl\Tenancy\Bootstrappers\QueueTenancyBootstrapper::class,
+        Stancl\Tenancy\Bootstrappers\SessionTenancyBootstrapper::class,
         // Stancl\Tenancy\Bootstrappers\RedisTenancyBootstrapper::class, // Note: phpredis is needed
     ],
 
